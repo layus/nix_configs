afd6e3eadf36a2d514d3e3f7d0db5c016b0f4b33
Guillaume Maudoux <layus.on@gmail.com>
2017-04-18 18:12:21 +0200

    update vbox config after tests on klatch

--- vbox_mozart.nix
+++ vbox_mozart.nix
@@ -8,23 +8,47 @@ in {
     <nixpkgs/nixos/modules/virtualisation/virtualbox-image.nix>
   ];
 
-  users.extraUsers.root.password = "root";
-  users.extraUsers.${user}.password = "";
+  users.mutableUsers = false;
+  users.users.root = {
+    password = "root";
+    openssh.authorizedKeys.keyFiles = [
+      ~/.ssh/id_ecdsa.pub
+    ];
+  };
+  users.users.${user} = {
+    password = "";
+    isNormalUser = true;
+  };
 
   environment.systemPackages = with pkgs; [
+    emacs
     mozart
   ];
 
   virtualisation.virtualbox.guest.enable = true;
 
+  services.openssh.enable = true;
+
   boot.initrd.checkJournalingFS = false;
 
-  services.xserver.displayManager.auto.enable = true;
-  services.xserver.displayManager.auto.user = "${user}";
+  services.xserver = {
+    enable = true;
+    displayManager.auto.enable = true;
+    displayManager.auto.user = "${user}";
+    windowManager.i3.enable = true;
+    windowManager.default = "i3";
+    desktopManager.default = "xterm";
+  };
 
+  # TODO
+  # 1. user's /nix/var/nix/profiles/per-user/user is not defined.
+  # 2. /tmp is not tmpfs and has missing write permissions (chmod a+wt /tmp).
+
+  /*
   fileSystems."/home/${user}/shared_files" = {
     fsType = "vboxsf";
     device = "shared_files";
     options = [ "rw" ];
   };
+  */
 }
